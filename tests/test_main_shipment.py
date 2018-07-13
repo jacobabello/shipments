6fee9895260b8959cfad3504ed27671811e8d032
jacob24 <abello.jacob@gmail.com>
2018-07-13 13:43:44 -0600

    test for shipment class

--- tests/test_main_shipment.py
+++ tests/test_main_shipment.py
@@ -1,5 +1,15 @@
 import unittest
+from shipments import generate_shipment
+from shipments.shipment import Shipment, Manifest, BillOfLading
 
 
 class test_main_shipment(unittest.TestCase):
-    pass
+    def test_generate_shipment_should_return_shipment_obj(self):
+        shipment = generate_shipment(BillOfLading(), Manifest())
+        self.assertIsInstance(shipment, Shipment)
+
+    def test_shipment_can_be_initialized_by_using_billoflading_and_manifest(self):
+        with self.assertRaises(ValueError):
+            generate_shipment(None, Manifest)
+        with self.assertRaises(ValueError):
+            generate_shipment(BillOfLading, None)
