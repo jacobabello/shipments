fc2601faacdd062c62d3d6fa8b74a00f1cd93c19
jacob24 <abello.jacob@gmail.com>
2018-07-13 13:42:45 -0600

    setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='shipment',
+    description='Simple OOP representation of shipments components',
+    author='abello.jacob@gmail.com',
+    packages=[
+        'shipments'
+    ]
+)
