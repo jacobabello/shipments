4c735a8b0d09f0002119bfdf7778de38714b7f0b
jacob24 <abello.jacob@gmail.com>
2018-07-13 17:45:05 -0600

    test for cargo classs

--- tests/test_cargo.py
+++ tests/test_cargo.py
@@ -1,5 +1,15 @@
 import unittest
 
+from shipments.cargo import Cargo
+from shipments.shipment_component import ShipmentComponent
+
 
 class test_cargo(unittest.TestCase):
-    pass
+    def test_container_should_be_object_of_components(self):
+        self.assertIsInstance(Cargo('001', 'Fake Cargo 1st Description', 10), ShipmentComponent)
+
+    def test_cargo_data(self):
+        cargo = Cargo('001', 'Fake Cargo 1st Description', 10)
+        self.assertEqual(cargo.get_sequence_number(), '001')
+        self.assertEqual(cargo.get_product_description(), 'Fake Cargo 1st Description')
+        self.assertEqual(cargo.get_piece_count(), 10)
