6f818055c48ac44136a602281d0ff8b2e1a61259
jacob24 <abello.jacob@gmail.com>
2018-07-13 18:45:15 -0600

    add tests for json return

--- tests/test_cargo.py
+++ tests/test_cargo.py
@@ -13,3 +13,12 @@ class test_cargo(unittest.TestCase):
         self.assertEqual(cargo.get_sequence_number(), '001')
         self.assertEqual(cargo.get_product_description(), 'Fake Cargo 1st Description')
         self.assertEqual(cargo.get_piece_count(), 10)
+
+    def test_json_return(self):
+        real_json = {
+            'Product description': 'Fake Cargo 1st Description',
+            'Piece count': 10
+        }
+
+        cargo = Cargo('001', 'Fake Cargo 1st Description', 10)
+        self.assertEqual(cargo.generate_json(), real_json)
--- tests/test_company.py
+++ tests/test_company.py
@@ -1,5 +1,23 @@
 import unittest
 
+from shipments.company import Company
+from shipments.shipment_component import ShipmentComponent
+
 
 class test_company(unittest.TestCase):
-    pass
+    def test_container_should_be_object_of_components(self):
+        self.assertIsInstance(Company('shipper Name', 'shipper Address'), ShipmentComponent)
+
+    def test_company_data(self):
+        company = Company('shipper Name', 'shipper Address')
+        self.assertEqual(company.get_name(), 'shipper Name')
+        self.assertEqual(company.get_address(), 'shipper Address')
+
+    def test_json_data(self):
+        real_json = {
+            'Name': 'shipper Name',
+            'Address': 'shipper Address'
+        }
+
+        cargo = Company('shipper Name', 'shipper Address')
+        self.assertEqual(cargo.generate_json(), real_json)
