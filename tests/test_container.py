49b8c1de880c8ddead280c9dde5c6cd78c66edd3
jacob24 <abello.jacob@gmail.com>
2018-07-14 18:19:08 -0600

    add test for json response

--- tests/test_container.py
+++ tests/test_container.py
@@ -29,16 +29,11 @@ class test_container(unittest.TestCase):
         with self.assertRaises(ValueError):
             container.set_type_of_service('ZZ')
 
-    def test_can_add_cargo(self):
-        container = Container('000001')
-        cargo = container.add_cargo('001', 'Fake Cargo Description', 10)
-        self.assertIsInstance(cargo, Cargo)
-
     def test_get_cargo_by_sequence_number(self):
         container = Container('000001')
-        cargo = container.add_cargo('001', 'Fake Cargo Description', 10)
+        container.add_cargo('001', 'Fake Cargo Description', 10)
 
-        self.assertEqual(container.get_cargo_by_sequence_number('001'), cargo)
+        self.assertIsInstance(container.get_cargo_by_sequence_number('001'), Cargo)
 
     def test_get_all_cargoes(self):
         container = Container('000001')
@@ -56,3 +51,84 @@ class test_container(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             container.add_cargo('001', 'Fake Cargo 1st Description', 10)
+
+    def test_json_data_single_container(self):
+        real_json = {
+            'Container Number': '000001',
+            'Length': 100,
+            'Height': 100,
+            'Width': 100,
+            'Load status': 'Loaded',
+            'Type of service': 'Break Bulk'
+        }
+
+        container = Container('000001')\
+            .set_height(100)\
+            .set_length(100)\
+            .set_width(100)\
+            .set_load_status('Loaded')\
+            .set_type_of_service('BB')
+
+        self.assertEqual(container.generate_json(), real_json)
+
+    def test_json_data_cargo(self):
+        real_json = {
+            'Container Number': '000001',
+            'Length': 100,
+            'Height': 100,
+            'Width': 100,
+            'Load status': 'Loaded',
+            'Type of service': 'Break Bulk',
+            'Cargoes': [
+                {
+                    'Product description': "Fake Product Description",
+                    'Piece count': 10
+                }
+            ]
+        }
+
+        container = Container('000001')
+        container.set_height(100)
+        container.set_length(100)
+        container.set_width(100)
+        container.set_load_status('Loaded')
+        container.set_type_of_service('BB')
+        container.add_cargo('001', 'Fake Product Description', 10)
+
+        self.assertEqual(container.generate_json(), real_json)
+
+    def test_json_data_multiple_cargo(self):
+        real_json = {
+            'Container Number': '000001',
+            'Length': 100,
+            'Height': 100,
+            'Width': 100,
+            'Load status': 'Loaded',
+            'Type of service': 'Break Bulk',
+            'Cargoes': [
+                {
+                    'Product description': "Fake Product Description 1",
+                    'Piece count': 10
+                },
+                {
+                    'Product description': "Fake Product Description 2",
+                    'Piece count': 20
+                },
+                {
+                    'Product description': "Fake Product Description 3",
+                    'Piece count': 30
+                }
+            ]
+        }
+
+        container = Container('000001')
+        container.set_height(100)
+        container.set_length(100)
+        container.set_width(100)
+        container.set_load_status('Loaded')
+        container.set_type_of_service('BB')
+        container.add_cargo('001', 'Fake Product Description 1', 10)
+        container.add_cargo('002', 'Fake Product Description 2', 20)
+        container.add_cargo('003', 'Fake Product Description 3', 30)
+
+        self.assertEqual(container.generate_json(), real_json)
