fa8cd6b100246d9a5cee5400535e200673e50e72
jacob24 <abello.jacob@gmail.com>
2018-07-14 18:16:00 -0600

    add fix for container

--- shipments/__init__.py
+++ shipments/__init__.py
@@ -1,5 +1,14 @@
 from .shipment import Shipment
+from .billoflading import BillOfLading
+from .manifest import Manifest
 
 
 def generate_shipment(billoflading, manifest):
+    """
+    :type billoflading BillOfLading
+    :type manifest Manifest
+    :rtype: Shipment
+    """
     return Shipment(billoflading, manifest)
+
+
